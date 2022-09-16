c1250240ae6034a6bc22ce17eda0e2e0d48b6786
a5a8a0a9 <a5a8a0a9@yahoo.com.tw>
2022-09-16 13:13:31 +0800

    deploy

---
+++ js/chunk-2d0d3a46.8493b399.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d3a46"],{"5e44":function(t,e,n){"use strict";n.r(e);var r=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"flex-row items-center content-center h-100 text-center"},[n("router-link",{staticStyle:{cursor:"pointer"},attrs:{to:"/",tag:"h1"}},[n("div",[t._v("你走錯路囉！")]),n("div",[t._v("請跟我回首頁一趟~")])])],1)},c=[],s={},i=s,a=n("2877"),l=Object(a["a"])(i,r,c,!1,null,null,null);e["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d0d3a46.8493b399.js.map
